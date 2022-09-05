2116789a793e5da7f1b6382c9c5f6083f22edfdc
nxm67420 <46074688+nxm67420@users.noreply.github.com>
2022-09-05 12:09:21 -0500

    Spring Created

---
+++ src/main/java/com/warehouse_quote/warehousequoteapp/WarehouseQuoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.warehouse_quote.warehousequoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseQuoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseQuoteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warehouse_quote/warehousequoteapp/WarehouseQuoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehouse_quote.warehousequoteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseQuoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
